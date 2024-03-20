3dc1f4856d3b90cfa86e33f0c9a1263ea5def986
sarita.bagul <sarita.bagul@growexx.com>
2024-03-20 11:14:14 +0530

    :ambulance: added new root router

--- router/router.go
+++ router/router.go
@@ -6,21 +6,22 @@ import (
 )
 
 func SetupRouter(noteController *controllers.NoteController) *gin.Engine {
-    router := gin.Default()
+	router := gin.Default()
 
-    // Initialize routes
-    api := router.Group("/api")
-    {
-        notes := api.Group("/notes")
-        {
-            notes.POST("/", noteController.CreateNoteHandler)
-            notes.PUT("/:id", noteController.UpdateNoteHandler)
-            notes.DELETE("/:id", noteController.DeleteNoteHandler)
-            notes.GET("/:id", noteController.GetNoteByIDHandler)
-            notes.GET("/", noteController.GetAllNotesHandler)
-            notes.GET("/search", noteController.SearchNotesHandler)
-        }
-    }
+	// Initialize routes
+	api := router.Group("/api")
+	{
+		notes := api.Group("/notes")
+		{
+			notes.POST("/", noteController.CreateNoteHandler)
+			notes.PUT("/:id", noteController.UpdateNoteHandler)
+			notes.DELETE("/:id", noteController.DeleteNoteHandler)
+			notes.GET("/:id", noteController.GetNoteByIDHandler)
+			notes.GET("/", noteController.GetAllNotesHandler)
+			notes.GET("/search", noteController.SearchNotesHandler)
+		}
+	}
 
-    return router
+	router.GET("/", noteController.GetAllNotesHandler)
+	return router
 }
